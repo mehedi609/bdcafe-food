c278bc1fe6fe739c7f6fdb6704a8352a9e8cd58b
mehediPC <mehedi609@gmail.com>
2022-08-28 14:41:05 +0600

    Implemented Layout and Navbar

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Layout from './components/Layout/Layout';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Layout />
+    </div>
+  );
+}
---
+++ src/components/Layout/Main/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Main.css';
+
+const Main = () => {
+  const renderMain = (props) => <div className="main">Main</div>;
+
+  return <>{renderMain()}</>;
+};
+
+export default Main;
